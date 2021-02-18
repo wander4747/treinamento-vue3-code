7c3c2a958960552174c65e74a0b2b9521c1bb42f
Wander <wander@bigbangcomunicacao.com.br>
2021-02-18 08:13:08 -0300

    test: create test utils/validator

---
+++ dashboard/src/utils/validators.spec.js
@@ -0,0 +1,30 @@
+import {
+  validateEmptyAndLength3,
+  validateEmptyAndEmail
+} from './validators'
+
+describe('Validators util', () => {
+  it('should give an error with with payload', () => {
+    expect(validateEmptyAndLength3()).toBe('*Este campo é obrigatório')
+  })
+
+  it('should give an error with less then 3 character payload', () => {
+    expect(validateEmptyAndLength3('12')).toBe('*Este campo precisa de no mínimo 3 caracteres')
+  })
+
+  it('should returns true when pass a correct param', () => {
+    expect(validateEmptyAndLength3('1234')).toBe(true)
+  })
+
+  it('should give an error with empty payload', () => {
+    expect(validateEmptyAndEmail()).toBe('*Este campo é obrigatório')
+  })
+
+  it('should give an error with a invalid param', () => {
+    expect(validateEmptyAndEmail('myemail@')).toBe('*Este campo precisa ser um e-mail')
+  })
+
+  it('should returns true when pass a correct param', () => {
+    expect(validateEmptyAndEmail('igor@igor.me')).toBe(true)
+  })
+})
